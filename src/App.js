6662b6c834584f1ba235a5e17d42419e9ea8ccd1
meenachinmay <chinmayanand896@gmail.com>
2020-07-30 08:49:41 +0530

    Initial commit, useState hook with array and normal usage

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  // USING REACT USESTATE HOOK NORMALLY
+  // const [product, setProduct] = useState('');
+  //
+  // const handleProductSubmit = (e) => {
+  //   e.preventDefault();
+  //
+  //   console.log(product);
+  // }
+
+  //  USING REACT useSTATE HOOKS WITH ARRAY
+  // const [names, setNames] = useState([]);
+  // const [firstname, setFirstname] = useState('');
+  // const [lastname, setLastname] = useState('');
+  // const [email, setEmail] = useState('');
+  //
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   setNames([
+  //     ...names,
+  //     {
+  //       id: Date.now(),
+  //       firstName: firstname,
+  //       lastName: lastname,
+  //       Email: email
+  //     }
+  //   ]);
+  // }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ margin: '50px' }}>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="Firatname" value={firstname} onChange={(e) => setFirstname(e.target.value)}/>
+        <input type="text" placeholder="Lastname" value={lastname} onChange={(e) => setLastname(e.target.value)}/>
+        <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)}/>
+        <button htmltype="submit" style={{ margin: '10px'}} onClick={handleSubmit}>Submit</button>
+      </form>
+      <ul>
+        {names.map((name) => (
+          <li key={name.id}>
+            {name.firstName}, {name.lastName}, {name.id}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
