5a6225087d9a3e2161b0ad0cc14ed0f29f143602
meenachinmay <chinmayanand896@gmail.com>
2020-07-30 10:00:56 +0530

    custom hook for form handling added

--- src/App.js
+++ src/App.js
@@ -1,52 +1,24 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { useFormFields } from './hooks/useForm';
 import './App.css';
 
 function App() {
 
-  // USING REACT USESTATE HOOK NORMALLY
-  // const [product, setProduct] = useState('');
-  //
-  // const handleProductSubmit = (e) => {
-  //   e.preventDefault();
-  //
-  //   console.log(product);
-  // }
+  const [values, handleFieldChange] = useFormFields({email: "", password: ""});
 
-  //  USING REACT useSTATE HOOKS WITH ARRAY
-  // const [names, setNames] = useState([]);
-  // const [firstname, setFirstname] = useState('');
-  // const [lastname, setLastname] = useState('');
-  // const [email, setEmail] = useState('');
-  //
-  // const handleSubmit = (e) => {
-  //   e.preventDefault();
-  //   setNames([
-  //     ...names,
-  //     {
-  //       id: Date.now(),
-  //       firstName: firstname,
-  //       lastName: lastname,
-  //       Email: email
-  //     }
-  //   ]);
-  // }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(values)
+  }
 
   return (
-    <div className="App" style={{ margin: '50px' }}>
-      <form onSubmit={handleSubmit}>
-        <input type="text" placeholder="Firatname" value={firstname} onChange={(e) => setFirstname(e.target.value)}/>
-        <input type="text" placeholder="Lastname" value={lastname} onChange={(e) => setLastname(e.target.value)}/>
-        <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)}/>
-        <button htmltype="submit" style={{ margin: '10px'}} onClick={handleSubmit}>Submit</button>
-      </form>
-      <ul>
-        {names.map((name) => (
-          <li key={name.id}>
-            {name.firstName}, {name.lastName}, {name.id}
-          </li>
-        ))}
-      </ul>
-    </div>
+      <div className="App" style={{ margin: '50px'}}>
+        <form onSubmit={handleSubmit}>
+          <input type="eamil" value={values.email} onChange={handleFieldChange} name="email"/>
+          <input type="password" value={values.password} onChange={handleFieldChange} name="password"/>
+          <button htmltype="submit">Submit</button>
+        </form>
+      </div>
   );
 }
 
